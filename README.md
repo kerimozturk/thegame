ee2e99b9e71b4f18c9249129260e61d3a0cb5560
Kerim Öztürk <kerim.ozturk@agmlab.com>
2014-04-04 11:05:20 +0300

    First commit

--- README.md
+++
@@ -1,4 +0,0 @@
-thegame
-=======
-
-Space Shooter
---
+++ SpaceShooter/Assets/Objects/Player/scriptPlayer.js
@@ -0,0 +1,57 @@
+﻿#pragma strict
+
+var speed : float = -3.0f;
+
+private var maxX : float;
+private var maxY : float;
+
+var bulletPrefab : GameObject;
+
+
+
+
+function Start () {
+	maxY = Camera.main.orthographicSize;
+	maxX = maxY * Camera.main.aspect;
+	maxX -= 1;
+	maxY -= 1;
+}
+
+function Update () {
+	var moveX : float;
+	var moveY : float;
+	
+	moveX = Input.GetAxis("Horizontal");
+	moveY = Input.GetAxis("Vertical");
+	
+	transform.Translate(moveX * speed * Time.deltaTime, 
+						moveY * speed * Time.deltaTime, 
+						0.0f);
+						
+	transform.position.x = Mathf.Clamp(
+							transform.position.x, 
+							-maxX, 
+							maxX);
+	
+	transform.position.y = Mathf.Clamp(
+							transform.position.y, 
+							-maxY, 
+							maxY);
+	
+	if(Input.GetKeyDown(KeyCode.Space)){
+		var barrelPosition = transform.FindChild("centerBarrel").transform.position;
+		
+		Instantiate(bulletPrefab, 
+				barrelPosition, 
+				Quaternion.identity);
+	}
+}
+
+function OnTriggerEnter (other : Collider) {
+	if(other.tag == "Asteroid")
+		Destroy(gameObject);
+}
+
+
+
+
